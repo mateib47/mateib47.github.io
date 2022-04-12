6f7bc75ff408f658fc059cea37e3d7de342c2434
mateib47 <m.bucur@student.utwente.nl>
2022-04-12 19:54:10 +0200

    add typing animation on intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,25 +1,41 @@
-import './intro.scss'
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      strings: ["Web Developer","Entrepreneur", "Negotiator"],
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true
+    });
+  }, []);
+
   return (
-    <div className='intro' id='home'>
+    <div className="intro" id="home">
       <div className="left">
         <div className="imgContainer">
           <img src="assets/profile1.png" alt="" />
         </div>
-      </div>  
+      </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Matei Bucur</h1>
-          <h3>UT Student<span></span></h3>
+          <h3>
+            Student <span ref={textRef}></span>
+          </h3>
         </div>
         <a href="#portofolio">
-          <img src="assets/down.png" alt="" />  
+          <img src="assets/down.png" alt="" />
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
