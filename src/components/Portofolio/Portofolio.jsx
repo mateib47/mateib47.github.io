4aa85a810293de5de9b2446c84db7d3fcc7ca37c
mateib47 <m.bucur@student.utwente.nl>
2022-04-13 20:55:57 +0200

    add portofolio page

--- src/components/Portofolio/Portofolio.jsx
+++ src/components/Portofolio/Portofolio.jsx
@@ -2,7 +2,42 @@ import './portofolio.scss'
 
 const Portofolio = () => {
   return (
-    <div className='portofolio' id='portofolio'>Portofolio</div>
+    <div className='portofolio' id='portofolio'>
+      <h1>Portofolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+        <li>Featured</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
+          <h3>Book app  </h3>
+        </div>
+        <div className="item">
+          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
+          <h3>Book app  </h3>
+        </div>
+        <div className="item">
+          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
+          <h3>Book app  </h3>
+        </div>
+        <div className="item">
+          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
+          <h3>Book app  </h3>
+        </div>
+        <div className="item">
+          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
+          <h3>Book app  </h3>
+        </div>
+        <div className="item">
+          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
+          <h3>Book app  </h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
