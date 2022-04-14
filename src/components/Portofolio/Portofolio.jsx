3014f79119906bdfc9566b1d4390263f4194a030
mateib47 <m.bucur@student.utwente.nl>
2022-04-14 23:15:36 +0200

    add dynamic active class and data in portfolio section

--- src/components/Portofolio/Portofolio.jsx
+++ src/components/Portofolio/Portofolio.jsx
@@ -1,41 +1,36 @@
 import './portofolio.scss'
+import PortfolioList from './PortofolioList/PortfolioList'
+import { useState } from 'react'
+import {
+  projectsList, sectionsList
+} from "../../data"
+import { useEffect } from 'react'
 
 const Portofolio = () => {
+  const [selected, setSelected] = useState("0");
+  const [data, setData] = useState([])
+  const list = sectionsList;
+
+  useEffect(() => {
+    setData(projectsList.find((elem) => elem.id == selected).items)
+  }, [selected])
+  
+
   return (
     <div className='portofolio' id='portofolio'>
       <h1>Portofolio</h1>
       <ul>
-        <li className='active'>Featured</li>
-        <li>Featured</li>
-        <li>Featured</li>
-        <li>Featured</li>
-        <li>Featured</li>
+        {list.map((item) => (
+          <PortfolioList title={item.title} id={item.id} active={selected === item.id} setSelected={setSelected} key={item.id}/>
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
-          <h3>Book app  </h3>
-        </div>
-        <div className="item">
-          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
-          <h3>Book app  </h3>
-        </div>
-        <div className="item">
-          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
-          <h3>Book app  </h3>
-        </div>
-        <div className="item">
-          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
-          <h3>Book app  </h3>
-        </div>
-        <div className="item">
-          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
-          <h3>Book app  </h3>
-        </div>
-        <div className="item">
-          <img src="http://mateibucur.me/images/baby-things.png" alt="" />
-          <h3>Book app  </h3>
-        </div>
+        {data.map((x) => (
+          <div className="item" key={x.id}>
+            <img src={x.img} alt="" />
+            <h3>{x.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projectsList = [
+  {
+    id: 0,
+    sectionTitle: "featured",
+    items: [
+      {
+        id: 1,
+        title: "Ecommerce app",
+        img: "http://mateibucur.me/images/baby-things.png",
+      },
+      {
+        id: 2,
+        title: "Book list app",
+        img: "http://mateibucur.me/images/book-list.PNG",
+      },
+    ],
+  },
+  {
+    id: 1,
+    sectionTitle: "featured",
+    items: [
+      {
+        id: 1,
+        title: "Ecommerce app",
+        img: "http://mateibucur.me/images/baby-things.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    sectionTitle: "featured",
+    items: [
+      {
+        id: 2,
+        title: "Book list app",
+        img: "http://mateibucur.me/images/book-list.PNG",
+      },
+    ],
+  },
+];
+
+export const sectionsList = [
+    {
+      id:"0",
+      title:"Featured"
+    },
+    {
+      id:"1",
+      title:"Featured2"
+    },
+    {
+      id:"2",
+      title:"Featured3"
+    },
+  ]
