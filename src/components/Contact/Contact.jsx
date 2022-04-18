6bd22b4e3aff93c7c690c251d765450b81ff16e1
mateib47 <m.bucur@student.utwente.nl>
2022-04-18 23:20:57 +0200

    add contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,28 @@
 import './contact.scss'
+import { useState } from 'react'
 
 const Contact = () => {
+  const [message, setMessage] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  }
+
   return (
-    <div className='contact' id='contact'>Contact</div>
+    <div className='contact' id='contact'>
+      <div className="left">
+        <img src="https://www.insidehighered.com/sites/default/server_files/media/barber%20handshake.jpg" alt="" />
+      </div>
+      <div className="right">
+        <h1>Contact.</h1>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" placeholder='Email' />
+          <textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
+          <button type='submit'>Send</button>
+          {message && <span>Thank you, I will reply soon!</span>}
+        </form>
+      </div>
+    </div>
   )
 }
 
