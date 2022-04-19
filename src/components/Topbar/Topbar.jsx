3502b1a4bc306598abd5f5435ac09843ac6f7303
mateib47 <m.bucur@student.utwente.nl>
2022-04-19 22:22:36 +0200

    add extra icons and links to the topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,7 +1,13 @@
 import "./topbar.scss";
-import {Person, Mail} from '@material-ui/icons'
+import {Person, Mail, LinkedIn, GitHub,Folder} from '@material-ui/icons'
 
 const Topbar = ({open, setOpen}) => {
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+    if (newWindow) newWindow.opener = null
+  }
+//fetch from server the contact details data and have a map that renders them
+//add cv and fetch it from server
   return (
     <div className={"topbar " + (open && "active")}>
       <div className="wrapper">
@@ -11,9 +17,27 @@ const Topbar = ({open, setOpen}) => {
             <Person className="icon"/>
             <span>+40 722 222 251</span>
           </div>
-          <div className="itemContainer">
+          <div className="itemContainer" onClick={(e) => {
+                openInNewTab('mailto:bucur.matei@yahoo.com')
+            }}>
             <Mail className="icon"/>
             <span>matei@bucur.com</span>
+          </div>
+          <div className="itemContainer" onClick={(e) => {
+                openInNewTab('https://www.linkedin.com/in/matei-bucur-57a591207/')
+            }}>
+            <LinkedIn className="icon"/>
+            <span>matei bucur</span>
+          </div>
+          <div className="itemContainer" onClick={(e) => {
+                openInNewTab('https://github.com/mateib47')
+            }}>
+            <GitHub className="icon"/>
+            <span>mateib47</span>
+          </div>
+          <div className="itemContainer">
+            <Folder className="icon"/>
+            <span>cv</span>
           </div>
         </div>
         <div className="right">
