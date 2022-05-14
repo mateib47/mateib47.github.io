96a4a67eda9f8ee277f2a110ac9c64970a876446
mateib47 <m.bucur@student.utwente.nl>
2022-05-14 23:59:38 +0200

    work on bolded words bug

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,25 +1,32 @@
 import "./about.scss";
 import { about } from "../../data";
+import { useEffect } from "react";
 
 const About = () => {
   const createDescription = () => {
     let s = about.description;
-    for (let word in about.boldedWords) {
-      let regex = new RegExp(word, "i");
-      if (s.word(regex) != -1) {
-        console.log("<pre>" + s.replace(regex, "<b>$&</b>") + "</pre>") ;
+    let w = about.boldedWords;
+    for (let i = 1;i< w.length;i++) {
+      let word = w[i];
+      let regex = new RegExp(w[i], "g");
+      s.replaceAll(regex, `<b>${w[i]}</b>`);
+        // if (s.word(regex) != -1) {
+        //   console.log("<pre>" + s.replace(regex, "<b>$&</b>") + "</pre>") ;
+            
+        // }
           
-      }
     }
     return s;
   };
+
+  
 //fixme
   return (
     <div className="about">
       <h1>About me</h1>
       <div className="container">
         <div className="left">
-          <div className="textContainer">{about.description}</div>
+          <div className="textContainer">{createDescription()}</div>
         </div>
         <div className="right">
           <img src={about.imgLink} alt="" />
